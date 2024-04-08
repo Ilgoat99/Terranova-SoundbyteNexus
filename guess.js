63fdf4a134f9d40dd12927a7413c4ed7e31234c0
katinii <yurii.krainianskyi@polomanettiporciatti.edu.it>
2024-04-09 00:14:00 +0200

    guess the song fatto a casa

--- guess.js
+++ guess.js
@@ -1,46 +1,48 @@
 var songs = [
     {
-       
         audio: 'audio/My Name Is .mp3',
         options: ['My Name is', 'Slim Shady', 'Love the way you lie', 'Lose yourself', 'Not afraid'],
         correctAnswer: 0 // Indice della risposta corretta nell'array delle opzioni
     },
     {
-        
         audio: 'audio/Lollipop .mp3',
-        options: ['My Name is', 'Slim Shady', 'Love the way you lie', 'Lose yourself', 'Not afraid'],
-        correctAnswer: 0 // Indice della risposta corretta nell'array delle opzioni
-    },
-
+        options: ['What do you mean', 'Slim Shady', 'Lollipop', 'Heartbreaker', 'Nevermind'],
+        correctAnswer: 2 // Indice della risposta corretta nell'array delle opzioni
+    }
 ];
 
+var currentSongIndex = -1;
+var score = 0;
+var totalScore = 0;
 var timerInterval;
 var timeElapsed = 0;
 
 function showGame() {
     var gameContainer = document.getElementById('gameContainer');
-    gameContainer.classList.remove('hidden');
+    gameContainer.style.display = "block";
 }
-
-var lastSongIndex = -1; // Variabile per tenere traccia dell'indice dell'ultima canzone riprodotta
 
 function startGame() {
     var gameArea = document.getElementById('gameArea');
-    gameArea.classList.remove('hidden');
+    gameArea.style.display = "block";
+
+    var songInfo = document.querySelector('.song-info');
+    songInfo.style.display = "none"; // Nasconde il div song-info
 
     var songIndex;
     do {
         songIndex = Math.floor(Math.random() * songs.length); // Scegli una canzone casualmente
-    } while (songIndex === lastSongIndex); // Continua a selezionare una nuova canzone finché non è diversa dall'ultima
+    } while (songIndex === currentSongIndex); // Continua a selezionare una nuova canzone finché non è diversa dall'ultima
 
-    lastSongIndex = songIndex; // Salva l'indice della nuova canzone
+    currentSongIndex = songIndex; // Salva l'indice della nuova canzone
 
     var song = songs[songIndex];
-    var songTitle = document.getElementById('songTitle');
+   
     var audio = document.getElementById('audio');
     var optionsContainer = document.getElementById('options');
     var result = document.getElementById('result');
     var timerElement = document.getElementById('timer');
+    var totalScoreElement = document.getElementById('totalScoreValue');
 
     // Resetta il timer e il conteggio dei secondi
     clearInterval(timerInterval);
@@ -49,6 +51,10 @@ function startGame() {
 
     songTitle.textContent = song.title;
     audio.src = song.audio;
+    audio.controls = 0; // Visualizza i controlli, inclusi il pulsante di pausa
+    
+    audio.disabled = false; // Riattiva il pulsante di riproduzione
+    audio.volume = 1; // Riattiva il suono
 
     // Rimuovi eventuali opzioni precedenti
     while (optionsContainer.firstChild) {
@@ -80,6 +86,10 @@ function startGame() {
 
     // Quando l'audio termina, seleziona e avvia una nuova canzone dopo un ritardo di 3 secondi
     audio.onended = function() {
+        // Aggiorna il punteggio totale
+        totalScore += score;
+        totalScoreElement.textContent = totalScore;
+
         // Resetta il timer e il conteggio dei secondi
         clearInterval(timerInterval);
         timeElapsed = 0;
@@ -88,15 +98,26 @@ function startGame() {
         setTimeout(startGame, 3000); // Dopo 3 secondi, avvia una nuova canzone
     };
 }
-
 function checkGuess(selectedIndex, correctIndex, resultElement) {
     var resultText;
     if (selectedIndex === correctIndex) {
         resultText = 'Correct!';
+        score = 30 - timeElapsed; // Calcola il punteggio sottraendo il tempo trascorso da 30 secondi
+        if (score < 0) {
+            score = 0; // Imposta il punteggio a 0 se il tempo trascorso è superiore a 30 secondi
+        }
+        totalScore += score; // Aggiorna il punteggio totale
+        document.getElementById('totalScoreValue').textContent = totalScore; // Aggiorna l'elemento HTML del punteggio totale
     } else {
         resultText = 'Wrong!';
     }
     resultElement.textContent = resultText;
+
+    // Disattiva i bottoni delle opzioni
+    var optionButtons = document.querySelectorAll('#options button');
+    optionButtons.forEach(function(button) {
+        button.disabled = true;
+    });
 
     // Interrompi la riproduzione dell'audio corrente
     var audio = document.getElementById('audio');
